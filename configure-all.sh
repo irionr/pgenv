653da8b0acf43960ab5422d89cb42d17114d69bb
Florin Irion <irionr@gmail.com>
2021-10-19 12:01:25 +0200

    no warning for missing-field-initializers

--- configure-all.sh
+++ configure-all.sh
@@ -34,7 +34,7 @@ then
     DEBUG_ARGS+=" --with-perl --enable-tap-tests"
 fi
 
-CFLAGS=" -Wall -Wextra -Wuninitialized -Wint-conversion -Wno-unused-parameter -Wno-sign-compare  -ggdb -Og -g3 -fno-omit-frame-pointer"
+CFLAGS=" -Wall -Wextra -Wuninitialized -Wint-conversion -Wno-unused-parameter -Wno-sign-compare  -ggdb -Og -g3 -fno-omit-frame-pointer -Wno-missing-field-initializers"
 
 # openSUSE and SUSE have tclConfig.sh in /usr/lib64 for x86_64 machines
 if [ -f "/etc/SuSE-release" ] && [ "$(uname -m)" == 'x86_64' ]
